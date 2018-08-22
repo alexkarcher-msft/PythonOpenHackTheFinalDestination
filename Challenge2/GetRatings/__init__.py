173caa45b691c7292f5143313203d7b7f99e71d5
Alex Karcher <alkarche@microsoft.com>
2018-08-22 13:56:24 -0700

    GetRatings preliminary implimented
    Not filtering by userId yet

--- Challenge2/GetRatings/__init__.py
+++ Challenge2/GetRatings/__init__.py
@@ -2,24 +2,16 @@ import logging
 
 import azure.functions as func
 
-
 def main(req: func.HttpRequest,
          docs: func.DocumentList) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('productId')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    name = req.params.get('userId')
 
     if name:
         return func.HttpResponse(docs[0].to_json())
     else:
         return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
+             "Please pass a userId on the query string",
              status_code=400
         )
